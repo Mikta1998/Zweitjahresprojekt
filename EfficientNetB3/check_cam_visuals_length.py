733de9334b17453a442cea88621a90f4364286c5
hsan.filip.timov <timov20168@hs-ansbach.de>
2025-06-18 06:17:10 +0200

    New changes

--- EfficientNetB3/check_cam_visuals_length.py
+++ EfficientNetB3/check_cam_visuals_length.py
@@ -6,8 +6,3 @@ image_count = len([f for f in os.listdir(cam_dir) if f.lower().endswith(('.png',
 print(f"Number of images in cam_visuals: {image_count}")
 
 
-checkpoint = torch.load("EfficientNetB3/train6/best_model_loss.pt", map_location="cpu")
-print(type(checkpoint))
-
-if isinstance(checkpoint, dict):
-    print("Checkpoint keys:", checkpoint.keys())
