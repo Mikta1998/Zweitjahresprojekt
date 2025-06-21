d85c5f5a9fa606ace35a603fcecce0e801945dd3
hsan.filip.timov <timov20168@hs-ansbach.de>
2025-06-21 11:15:16 +0200

    New Changes

--- EfficientNetB4/efficientnetb4.py
+++ EfficientNetB4/efficientnetb4.py
@@ -14,7 +14,7 @@ num_classes = 7
 batch_size = 16
 learning_rate = 1e-4
 num_epochs = 50
-patience = 15
+patience = 10
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # === Transforms for EfficientNet‑B4 ===
